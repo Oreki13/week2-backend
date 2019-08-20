9ac08e142484f75de23f816d1b08ccbe7e31d388
Oreki13 <arfandynugraha21@gmail.com>
2019-08-21 02:20:26 +0700

    403 helper

--- src/Helpers/formResponse.js
+++ src/Helpers/formResponse.js
@@ -5,5 +5,22 @@ module.exports = {
             result,
         };
         res.json(formResponse);
-    }
+    },
+    put: (res, status, result) => {
+        if (result.affectedRows !== 1) {
+            const formResponse = {
+                status: 'Not Found Param',
+                res: 'not Found'
+            }
+            res.json(formResponse, 403)
+
+        } else {
+            const formResponse = {
+                status,
+                result,
+            };
+            res.json(formResponse);
+
+        }
+    },
 }
--- src/Models/store.js
+++ src/Models/store.js
@@ -1,100 +1,100 @@
 const db = require("../Config/dbConnect");
 module.exports = {
-  getAllStore: () => {
-    return new Promise((resole, reject) => {
-      db.query("SELECT * FROM store", (error, response) => {
-        if (!error) {
-          resole(response);
-        } else {
-          reject(error);
-        }
-      });
-    });
-  },
-  getStoreItem: name => {
-    return new Promise((resolve, reject) => {
-      db.query(
-        "SELECT * FROM store WHERE name=?",
-        [name],
-        (error, response) => {
-          if (!error) {
-            resolve(response);
-          } else {
-            reject(error);
-          }
-        }
-      );
-    });
-  },
-  postStore: body => {
-    return new Promise((resolve, reject) => {
-      db.query(
-        "INSERT INTO store SET name=?, type=?, quantity=?, branch=?",
-        [body.name, body.type, body.quantity, body.branch],
-        (error, response) => {
-          if (!error) {
-            resolve(response);
-          } else {
-            reject(error);
-          }
-        }
-      );
-    });
-  },
-  deleteItem: id => {
-    return new Promise((resolve, reject) => {
-      db.query("DELETE FROM store WHERE id=?", [id], (error, response) => {
-        if (!error) {
-          resolve(response);
-        } else {
-          reject(error);
-        }
-      });
-    });
-  },
-  updateItem: body => {
-    return new Promise((resolve, reject) => {
-      db.query(
-        "UPDATE store SET name=?, type=?, quantity=?, branch=? WHERE id=?",
-        [body.name, body.type, body.quantity, body.branch, body.id],
-        (error, response) => {
-          if (!error) {
-            resolve(response);
-          } else {
-            reject(error);
-          }
-        }
-      );
-    });
-  },
-  sortByType: type => {
-    return new Promise((resolve, reject) => {
-      db.query(
-        "SELECT * FROM store WHERE type=?",
-        [type],
-        (error, response) => {
-          if (!error) {
-            resolve(response);
-          } else {
-            reject(error);
-          }
-        }
-      );
-    });
-  },
-  sortByBranch: branch => {
-    return new Promise((resolve, reject) => {
-      db.query(
-        "SELECT * FROM store WHERE branch=?",
-        [branch],
-        (error, response) => {
-          if (!error) {
-            resolve(response);
-          } else {
-            reject(error);
-          }
-        }
-      );
-    });
-  }
+    getAllStore: () => {
+        return new Promise((resole, reject) => {
+            db.query("SELECT * FROM store", (error, response) => {
+                if (!error) {
+                    resole(response);
+                } else {
+                    reject(error);
+                }
+            });
+        });
+    },
+    getStoreItem: name => {
+        return new Promise((resolve, reject) => {
+            db.query(
+                "SELECT * FROM store WHERE name=?",
+                [name],
+                (error, response) => {
+                    if (!error) {
+                        resolve(response);
+                    } else {
+                        reject(error);
+                    }
+                }
+            );
+        });
+    },
+    postStore: body => {
+        return new Promise((resolve, reject) => {
+            db.query(
+                "INSERT INTO store SET name=?, type=?, quantity=?, branch=?",
+                [body.name, body.type, body.quantity, body.branch],
+                (error, response) => {
+                    if (!error) {
+                        resolve(response);
+                    } else {
+                        reject(error);
+                    }
+                }
+            );
+        });
+    },
+    deleteItem: id => {
+        return new Promise((resolve, reject) => {
+            db.query("DELETE FROM store WHERE id=?", [id], (error, response) => {
+                if (!error) {
+                    resolve(response);
+                } else {
+                    reject(error);
+                }
+            });
+        });
+    },
+    updateItem: body => {
+        return new Promise((resolve, reject) => {
+            db.query(
+                "UPDATE store SET name=?, type=?, quantity=?, branch=? WHERE id=?",
+                [body.name, body.type, body.quantity, body.branch, body.id],
+                (error, response) => {
+                    if (!error) {
+                        resolve(response);
+                    } else {
+                        reject(error);
+                    }
+                }
+            );
+        });
+    },
+    sortByType: type => {
+        return new Promise((resolve, reject) => {
+            db.query(
+                "SELECT * FROM store WHERE type=?",
+                [type],
+                (error, response) => {
+                    if (!error) {
+                        resolve(response);
+                    } else {
+                        reject(error);
+                    }
+                }
+            );
+        });
+    },
+    sortByBranch: branch => {
+        return new Promise((resolve, reject) => {
+            db.query(
+                "SELECT * FROM store WHERE branch=?",
+                [branch],
+                (error, response) => {
+                    if (!error) {
+                        resolve(response);
+                    } else {
+                        reject(error);
+                    }
+                }
+            );
+        });
+    }
 };
